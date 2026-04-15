2fb38a82101367fe7751668469b5e47ce68a6b6e
M7N2 <empty.shell@yahoo.com>
2026-04-15 16:55:12 +0300

    Update test_bank_account.py

--- test_bank_account.py
+++ test_bank_account.py
@@ -1,4 +1,4 @@
-# Test banl_account.py
+# Test bank_account.py
 import unittest
 from bank_account import BankAccount
 
@@ -9,24 +9,24 @@ class BankTestCase(unittest.TestCase):
         self.new_owner = BankAccount('ronald reigan', 1000)
 
     def test_deposit(self):
-        """Проверка пополнения баланса"""
+        """Checking balance replenishment"""
         self.new_owner.deposit(5000)
         self.assertEqual(self.new_owner.balance, 6000)
 
     def test_withdraw(self):
-        """Проверяем снятие"""
+        """Checking withdrawals"""
         self.new_owner.withdraw(1000)
         self.assertEqual(self.new_owner.balance, 0)
 
     def test_withdraw_negative(self):
-        """Проверяем снятие при недостаточном балансе"""
+        """Checking withdrawals with insufficient balance"""
         initial_balance = self.new_owner.balance
         result = self.new_owner.withdraw(1500)
         self.assertEqual(self.new_owner.balance, initial_balance)
         self.assertEqual(result, "Not enoung money")
 
     def test_get_balance(self):
-        """Проверка баланса"""
+        """Checking balance"""
         result = self.new_owner.get_balance()
         self.assertEqual(result, 1000)            
 
